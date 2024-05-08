1d4585efbecbf61927fc347f88c018c31d2f319d
cosmic <144744086+ThatGuyAstral@users.noreply.github.com>
2024-05-08 16:21:40 -0400

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -8,49 +8,14 @@ namespace Cyanide
     {
         private bool isDragging;
         private Point lastcurpos;
-        private string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-        private string dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        public string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        public string dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
         public CyanideWindow()
         {
             if (!Directory.Exists(dir + @"\Projects") && !Directory.Exists(appData + @"\Cyanide"))
             {
-                MessageBox.Show("Cyanide is being setup.", "Cyanide", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                Directory.CreateDirectory(dir + @"\Projects");
-                Directory.CreateDirectory(appData + @"\Cyanide");
-                Directory.CreateDirectory(appData + @"\Cyanide\ProjectTemplate");
-                Directory.CreateDirectory(appData + @"\Cyanide\Editor");
-                using (WebClient webcli = new())
-                {
-                    try
-                    {
-                        webcli.DownloadFile("https://raw.githubusercontent.com/ThatGuyAstral/cyanide-template-project/main/kernel.cpp", appData + @"\Cyanide\ProjectTemplate\kernel.cpp");
-                        webcli.DownloadFile("https://raw.githubusercontent.com/ThatGuyAstral/cyanide-template-project/main/boot.s", appData + @"\Cyanide\ProjectTemplate\boot.s");
-                        webcli.DownloadFile("https://raw.githubusercontent.com/ThatGuyAstral/cyanide-template-project/main/link.ld", appData + @"\Cyanide\ProjectTemplate\link.ld");
-                        webcli.DownloadFile("https://raw.githubusercontent.com/ThatGuyAstral/Cyanide/main/CyanideDevelopmentEnvironment/UI.html", appData + @"\Cyanide\Editor\UI.html");
-                        webcli.DownloadFile("https://raw.githubusercontent.com/ThatGuyAstral/Cyanide/main/CyanideDevelopmentEnvironment/index.js", appData + @"\Cyanide\Editor\index.js");
-                    }
-                    catch (Exception ex)
-                    {
-                        if (MessageBox.Show($"ERROR: {ex.Message}", "Cyanide", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error) == DialogResult.Ignore)
-                        {
-                            MessageBox.Show("Cyanide failed to install a critical component. If you continue, functionality will be limited.");
-                        }
-                        if (MessageBox.Show($"ERROR: {ex.Message}", "Cyanide", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error) == DialogResult.Abort)
-                        {
-                            MessageBox.Show("Cyanide failed to install a critical component.");
-                            Close();
-                        }
-                        if (MessageBox.Show($"ERROR: {ex.Message}", "Cyanide", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error) == DialogResult.Retry)
-                        {
-                            webcli.DownloadFile("https://raw.githubusercontent.com/ThatGuyAstral/cyanide-template-project/main/kernel.cpp", appData + @"\Cyanide\ProjectTemplate\kernel.cpp");
-                            webcli.DownloadFile("https://raw.githubusercontent.com/ThatGuyAstral/cyanide-template-project/main/boot.s", appData + @"\Cyanide\ProjectTemplate\boot.s");
-                            webcli.DownloadFile("https://raw.githubusercontent.com/ThatGuyAstral/cyanide-template-project/main/link.ld", appData + @"\Cyanide\ProjectTemplate\link.ld");
-                            webcli.DownloadFile("https://raw.githubusercontent.com/ThatGuyAstral/Cyanide/main/CyanideDevelopmentEnvironment/UI.html", appData + @"\Cyanide\Editor\UI.html");
-                            webcli.DownloadFile("https://raw.githubusercontent.com/ThatGuyAstral/Cyanide/main/CyanideDevelopmentEnvironment/index.js", appData + @"\Cyanide\Editor\index.js");
-                        }
-                    }
-                }
+                Program.SetupCyanide();
             }
             InitializeComponent();
             BlocklyWindow.Hide();
@@ -92,12 +57,10 @@ namespace Cyanide
 
         }
 
-        // these don't have proper functionality on purpose
         private void NewProjectBtn_Click(object sender, EventArgs e)
         {
             using (FolderBrowserDialog fbd = new FolderBrowserDialog())
             {
-
 
             }
         }
@@ -111,34 +74,7 @@ namespace Cyanide
             WindowTitle.Text = "Untitled project - Cyanide";
             this.Controls.Add(BlocklyWindow);
             await BlocklyWindow.EnsureCoreWebView2Async(null);
-            string Blocks = "",ToolBox = "";
-            Dictionary<string, List<string>> CustomToolbox = new();
-            foreach (var category in Directory.GetDirectories(dir + @"\CyanideDevelopmentEnvironment\Blocks\Blocks"))
-            {
-
-                CustomToolbox.Add(Path.GetFileNameWithoutExtension(category),new());
-
-                foreach (var block in Directory.GetFiles(category))
-                {
-                    Blocks += $"Blockly.defineBlocksWithJsonArray([{File.ReadAllText(block)}]);\n";
-                    Blocks += $"Gen.forBlock['{Path.GetFileNameWithoutExtension(block)}'] = {File.ReadAllText(dir + $@"\CyanideDevelopmentEnvironment\Blocks\JS\{Path.GetFileNameWithoutExtension(block)}.js")}\n";
-
-                    CustomToolbox[Path.GetFileNameWithoutExtension(category)].Add(Path.GetFileNameWithoutExtension(block));
-
-                }
-            }
-            
-            foreach (var category in CustomToolbox)
-            {
-                ToolBox += $"<category name=\"{category.Key}\" colour=\"#5b80a5\">\n";
-                foreach (var block  in category.Value)
-                {
-                    ToolBox += $"<block type=\"{block}\"/>\n";
-                }
-                ToolBox += $"<\\category>\n";
-
-            }
-            BlocklyWindow.CoreWebView2.NavigateToString(File.ReadAllText(dir + @"\CyanideDevelopmentEnvironment\UI.html").Replace("%BlocksLoader%",Blocks).Replace("%ToolBoxLoader%", ToolBox));
+            BlocklyWindow.CoreWebView2.Navigate(appData + @"\Cyanide\Editor\UI.html");
             BlocklyWindow.Show();
         }
     }
