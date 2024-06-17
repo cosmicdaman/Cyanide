d9d0f072dfcfdae98eaeba907b268d12bf5aad4c
cosmic <144744086+ThatGuyAstral@users.noreply.github.com>
2024-06-17 19:01:13 -0400

    Create toolbox.hpp

---
+++ Linux/include/toolbox.hpp
@@ -0,0 +1,17 @@
+#ifndef TOOLBOX_HPP
+#define TOOLBOX_HPP
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <fstream>
+#include <filesystem>
+#include <cstring>
+
+class toolbox
+{
+	public:
+		void refresh();	
+};
+
+#endif
